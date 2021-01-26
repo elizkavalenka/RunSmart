9b65313ee2cd26ad346f98f879c5a7d0fbf2d809
elizkovalenko <67338086+elizkovalenko@users.noreply.github.com>
2021-01-26 16:36:09 +0300

    Update script.js
    add Smooth scroll animation

--- src/js/script.js
+++ src/js/script.js
@@ -108,4 +108,10 @@ $(document).ready(function() {
 			$('.pageup').fadeOut();
 		}
 	});
+	
+	$("a[href^='#']").click(function() {
+		const _href = $(this).attr("href");
+	$("html,body").animate({scrollTop:$(_href).offset().top+"px"});
+		return false;
+	});
 });
