5afb1bc2e599a92198b7cf8a9a9aaf78735e144a
elizkovalenko <elizkavalenka@gmail.com>
2021-01-27 21:26:11 +0300

    fix problem

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('kovalenko.elizabeth@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('menep19908@1adir.com');     // Add a recipient
+$mail->addAddress('nojej76346@jentrix.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
